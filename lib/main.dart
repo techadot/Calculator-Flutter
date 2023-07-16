2dabb902c5195f0032856e073952898f2a1faafe
Samuel Odukoya <samuelodukoya14@gmail.com>
2023-07-16 10:25:59 +0100

    worked on the Splash_Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:tbib_splash_screen/splash_screen_view.dart';
 import 'package:calculator_app/screens/calculator_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,10 +8,29 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    await Future.delayed(const Duration(seconds: 4));
+    setState(() {
+      _isLoaded = true;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,7 +40,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'NotoSans',
       ),
-      home: const CalculatorScreen(),
+      home: SplashScreenView(
+        navigateWhere: _isLoaded,
+        navigateRoute: const CalculatorScreen(),
+        text: WavyAnimatedText(
+          "Czarify Ft Aptlearn.io",
+          textStyle: const TextStyle(
+            color: Colors.blue,
+            fontSize: 32.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        imageSrc: "assets/splash_screen.png",
+      ),
     );
   }
 }
